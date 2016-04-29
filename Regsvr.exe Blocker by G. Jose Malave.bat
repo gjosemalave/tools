60e6c4edda93a3d76195b8211d614c5991ad0942
G. Jose Malave <gmelectronics365@gmail.com>
2016-04-29 09:41:30 -0400

    Update Regsvr.exe Blocker by G. Jose Malave.bat

--- Regsvr.exe Blocker by G. Jose Malave.bat
+++ Regsvr.exe Blocker by G. Jose Malave.bat
@@ -1,4 +1,5 @@
-:: BatchGotAdmin (Run as Admin code starts)
+::(Run as Admin code starts)
+:GotAdmin 
 REM --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 REM --> If error flag set, we do not have admin.
